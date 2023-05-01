c3971cfe9cf76ee56b968f588cf506b579ba9472
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2023-05-01 10:46:09 +0300

    sendemail

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const bodyParser = require("body-parser");
+
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(require("cors")());
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+app.get("/sendemail", async (req, res, next) => {
+  const email = process.env.EMAIL;
+  const transponter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: email,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const option = {
+    from: email,
+    to: email,
+    subject: "Замовлення",
+    text: "Ви отримали нове замовлення",
+  };
+  transponter.sendMail(option, function (error, info) {
+    if (error) {
+      console.log(error.message, "error");
+    } else {
+      console.log("mail sent", info);
+    }
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact page</h1>");
-});
-
-app.get("/contact/:id", (req, res) => {
-  res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`);
-});
-
-// app.patch("/user/:userid", (req, res) => {
-//   const id = req.params.userid;
-//   console.log(id);
-// });
+//localhost:3000/contact
 
 app.use((req, res, next) => {
   console.log("Наше проміжне ПЗ");
   next();
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+const port = process.env.PORT;
+
+app.listen(port || 3000, () => {
+  console.log("Example app");
 });
