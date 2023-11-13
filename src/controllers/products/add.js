5fed4f5d2fbd2dc88096c5c70bf23d21d70c597b
Yevhenns <gifestum@gmail.com>
2023-11-13 18:46:44 +0200

    req body

--- src/controllers/products/add.js
+++ src/controllers/products/add.js
@@ -1,7 +1,7 @@
 const { Product } = require("../../models");
 
 const add = async (req, res) => {
-  const result = await Product.create();
+  const result = await Product.create(req.body);
   res.status(201).json({
     status: "success",
     code: 201,
